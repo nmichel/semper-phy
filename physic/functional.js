f6296f0340a2d5e65f93916feb136e192abf8a04
Nicolas <nicolas.michel.lava@gmail.com>
2023-11-21 14:14:40 +0100

    Convert defprotocol and defimpl to Typescript

--- physic/functional.js
+++ physic/functional.js
@@ -1,3 +1,5 @@
+import { defprotocol, defimpl } from './functional.ts';
+
 const groupBy = (items, splitFn) => 
   items.reduce((acc, e) => {
     const splitKey = splitFn(e);
@@ -5,34 +7,6 @@ const groupBy = (items, splitFn) =>
     acc[splitKey] = [...accForKey, e];
     return acc;
   }, {});
-
-const defprotocol = (protocolName, funs) => {
-  const impls = {};
-  const prot = {name: protocolName, __impls: impls};
-
-  return funs.reduce((def, name) => {
-    def[name] = (obj, ...args) => {
-      const klass = obj.__proto__.constructor.name;
-      const protImpl = impls[klass];
-      if (!protImpl) {
-        throw new Error(`No implementation of protocol ${prot.name} for class ${klass}`);
-      }
-      const fn = protImpl[name];
-      if (!fn) {
-        throw new Error(`Cannot find impl of ${prot.name}.${name} for class ${klass}`);
-      }
-
-      return fn(obj, ...args);
-    }
-    return def;
-  }, prot);
-};
-
-const defimpl = ({__impls}, klass, name, fn) => {
-  const impl = __impls[klass.name] || {};
-  impl[name] = fn;
-  __impls[klass.name] = impl;
-};
 
 export { groupBy, defprotocol, defimpl };
 
---
+++ physic/functional.ts
@@ -0,0 +1,106 @@
+type ClassType<T> = new () => T;
+
+type AnyFunction = (...args: any[]) => any; // Is not the same as type Function
+type Interface = { [key: string]: AnyFunction };
+
+type ProtocolImpl<Type extends Interface, Target> = {
+    [Key in keyof Type]: (obj: Target, ...args: Parameters<Type[Key]>) => ReturnType<Type[Key]>
+}
+
+type ImplementationRegistry<Type extends Interface> = {
+    [key: string]: ProtocolImpl<Type, any>
+}
+
+type ProtocolInternals<Type extends Interface> = {
+    name: string,
+    __impls: ImplementationRegistry<Type>,
+}
+
+type Protocol<Type extends Interface> = ProtocolInternals<Type> & ProtocolImpl<Type, any>
+
+/**
+ * 
+ * @param protocolName 
+ * @param funs 
+ * @returns 
+ * 
+    const TestProtocolDef = {
+        foo: (): void => {},
+        neh: (_dummy: number): void => {}
+    }
+
+    const TestProtocol = defprotocol("Test", TestProtocolDef);
+
+    class Foo {
+        x: number = 0;
+    }
+
+    defimpl(TestProtocol, Foo, {
+        foo: (foo: Foo): void => {
+            console.log("foo", foo) ;
+        },
+        neh: (foo: Foo, number: number): void => {
+            console.log("neh", foo, number) ;
+        }
+    });
+
+    TestProtocol.foo(new Foo())
+    TestProtocol.neh(new Foo(), 42)
+
+    class Neh {
+        y: number = 0;
+    }
+
+    defimpl(TestProtocol, Neh, {
+        foo: (foo: Neh) => {
+            console.log("foo", foo) ;
+        },
+        neh: (foo: Neh, number: number) => {
+            console.log("neh", foo, number) ;
+        }
+    });
+
+    TestProtocol.foo(new Neh())
+    TestProtocol.neh(new Neh(), 42)
+
+ */
+export function defprotocol<Type extends Interface>(protocolName: string, funs: Type) : Protocol<Type>{
+  const impls: ImplementationRegistry<Type> = {};
+  const prot: Protocol<Type> = {name: protocolName, __impls: impls} as Protocol<Type>;
+  const keys: [keyof Type] = Object.keys(funs) as unknown as [keyof Type];
+
+  return keys.reduce((def: Protocol<Type>, name: keyof Type) => {
+    const stubFn = ((obj: Function, ...args: Parameters<Type[typeof name]>) => {
+      const klass: string = obj.constructor.name;
+      const protImpl: ProtocolImpl<Type, any> = impls[klass];
+      if (!protImpl) {
+        throw new Error(`No implementation of protocol ${prot.name} for class ${klass}`);
+      }
+      const fn = protImpl[name];
+      if (!fn) {
+        throw new Error(`Cannot find impl of ${prot.name}.${String(name)} for class ${klass}`);
+      }
+
+      return fn(obj, ...args);
+    });
+    def[name] = stubFn as unknown as Protocol<Type>[typeof name];
+    return def;
+  }, prot);
+};
+
+export function defimpl<Type extends Interface, Target>(protocol: Protocol<Type>, klass: ClassType<Target>, def: ProtocolImpl<Type, Target>) {
+    const __impls = protocol.__impls;
+    const keys: [keyof Type] = Object.keys(def) as unknown as [keyof Type];
+    keys.forEach(key => {
+        const impl = __impls[klass.name] || {};
+        impl[key] = def[key];
+        __impls[klass.name] = impl;
+    })
+};
+
+export class NotImplementedError extends Error {
+  constructor() {
+    super("Not implemented");
+  }
+}
+  
